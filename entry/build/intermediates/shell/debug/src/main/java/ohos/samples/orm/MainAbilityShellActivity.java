4002a903b0c86eee81fcaab6d9289b144a233b3b
unknown <pruenature@163.com>
2021-07-20 23:26:40 +0800

    init

---
+++ entry/build/intermediates/shell/debug/src/main/java/ohos/samples/orm/MainAbilityShellActivity.java
@@ -0,0 +1,11 @@
+package ohos.samples.orm;
+
+import android.os.Bundle;
+import ohos.abilityshell.AbilityShellActivity;
+
+public class MainAbilityShellActivity extends AbilityShellActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ entry/build/intermediates/shell/debug/src/main/java/ohos/samples/orm/ShellMyApplication.java
@@ -0,0 +1,10 @@
+package ohos.samples.orm;
+
+import ohos.abilityshell.HarmonyApplication;
+
+public class ShellMyApplication extends HarmonyApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
